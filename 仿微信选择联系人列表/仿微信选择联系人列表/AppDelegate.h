01694fe2c282c0355c345565179b51c34e6a55de
617 <review617@163.com>
2017-12-27 19:40:04 +0800

    create a project

---
+++ 仿微信选择联系人列表/仿微信选择联系人列表/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  仿微信选择联系人列表
+//
+//  Created by yongda on 2017/12/27.
+//  Copyright © 2017年 TangyuanLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 仿微信选择联系人列表/仿微信选择联系人列表/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  仿微信选择联系人列表
+//
+//  Created by yongda on 2017/12/27.
+//  Copyright © 2017年 TangyuanLiu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
